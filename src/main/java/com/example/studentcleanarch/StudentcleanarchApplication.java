38e210d636438342c988e21a5a44aff4175bb9d0
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2023-06-26 13:15:48 +0700

    init project

---
+++ src/main/java/com/example/studentcleanarch/StudentcleanarchApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentcleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentcleanarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentcleanarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentcleanarch/StudentcleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentcleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentcleanarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
