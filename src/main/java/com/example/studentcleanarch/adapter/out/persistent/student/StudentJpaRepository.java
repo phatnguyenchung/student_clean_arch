2029afb5de1e0c1d8cccaf0132ca772bcfdaeac6
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2023-06-28 08:10:42 +0700

    search student name by method like

--- src/main/java/com/example/studentcleanarch/adapter/out/persistent/student/StudentJpaRepository.java
+++ src/main/java/com/example/studentcleanarch/adapter/out/persistent/student/StudentJpaRepository.java
@@ -2,11 +2,13 @@ package com.example.studentcleanarch.adapter.out.persistent.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface StudentJpaRepository extends JpaRepository<StudentJpaEntity, Long>, JpaSpecificationExecutor<StudentJpaEntity> {
+    @Query(value = "SELECT * FROM tblStudent WHERE studentName LIKE %?1%",nativeQuery = true)
     Optional<StudentJpaEntity> findByStudentName(String studentName);
 }
