dfd68f1873f1f00ca8ce754cee20e2eb05be9c19
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2023-07-02 12:34:19 +0700

    modify response search student name

--- src/main/java/com/example/studentcleanarch/adapter/in/controller/StudentController.java
+++ src/main/java/com/example/studentcleanarch/adapter/in/controller/StudentController.java
@@ -43,8 +43,8 @@ public class StudentController {
     }
 
     @GetMapping("/searchstudentbyname")
-    public ApiResponse<?> searchstudentname(@RequestParam String studentName){
-        return ApiResponse.success(searchStudentUseCase.searchStudentByStudentName(studentName));
+    public ResponseEntity<Object> searchstudentname(@RequestParam String studentName){
+        return new ResponseEntity<Object>(searchStudentUseCase.searchStudentByStudentName(studentName),HttpStatus.OK);
     }
 
     @GetMapping("/searchstudentbyphonenumber")
