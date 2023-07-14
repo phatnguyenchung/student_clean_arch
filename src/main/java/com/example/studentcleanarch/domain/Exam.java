7cf1961f0c33e7cdb31de59e17c9acb679aad011
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2023-07-14 19:09:41 +0700

    create exam table

---
+++ src/main/java/com/example/studentcleanarch/domain/Exam.java
@@ -0,0 +1,18 @@
+package com.example.studentcleanarch.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Builder
+@Getter
+@Setter
+public class Exam {
+    private Long id;
+    private Long studentId;
+    private Long subjectId;
+    private Date examDate;
+    private int score;
+}
