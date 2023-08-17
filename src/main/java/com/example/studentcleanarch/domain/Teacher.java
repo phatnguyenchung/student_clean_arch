1e9c250d1bbaf756075b0f7dc7b7b42ef29d2dcc
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2023-08-17 11:58:27 +0700

    add teacher class

---
+++ src/main/java/com/example/studentcleanarch/domain/Teacher.java
@@ -0,0 +1,27 @@
+package com.example.studentcleanarch.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Builder
+@Getter
+@Setter
+public class Teacher {
+    private Long id;
+    private Long teacherId;
+    private String teacherFirstName;
+    private String teacherLastName;
+    private Date joinDate;
+    private String gender;
+    private String cic;
+    private String issuePlace;
+    private Date issueDate;
+    private Date expiredDate;
+    private Date birthDate;
+    private String nationality;
+    private String address;
+    private boolean active;
+}
