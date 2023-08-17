bee65cd8275d123f86a3c8ce32577e55436f0926
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2023-08-17 14:15:38 +0700

    delete subject by id

--- src/main/java/com/example/studentcleanarch/application/port/in/subject/DeleteSubjectCommand.java
+++ src/main/java/com/example/studentcleanarch/application/port/in/subject/DeleteSubjectCommand.java
@@ -5,18 +5,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
-import javax.validation.constraints.NotBlank;
-import java.util.Date;
-
 @Getter
 @Builder
 @ToString
 @EqualsAndHashCode
 public class DeleteSubjectCommand {
     private Long id;
-    @NotBlank
-    private Long subjectId;
-    private String subjectName;
-    private Date startDate;
-    private Date endDate;
 }
